a8da63f1426d3faee6472b3a1be9c1c5847cdf73
MariiaSvitsa <mariia0198@gmail.com>
2023-11-03 12:34:51 +0200

    +

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -11,14 +11,30 @@ import { TransactionHistory } from './TransactionHistory/TransactionHistory';
 import transactions from '../data/transactions.json';
 
 
+// export const App = () => {
+//   return (
+//     <>
+//       <Profile {...user} />
+//       <Statistics title="Upload stats" stats={data} />
+//       <FriendList friends={friends} />
+//       <TransactionHistory items={transactions} />;
+//     </>
+//   );
+// };
+
 export const App = () => {
   return (
     <>
-      <Profile {...user} />
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
       <Statistics title="Upload stats" stats={data} />
       <FriendList friends={friends} />
       <TransactionHistory items={transactions} />;
     </>
   );
-};
-
+}; 
